cb9496c160cf3589d1589641fbb486f3934fedb2
Emilio Medina <medinarribas@gmail.com>
2021-03-21 18:06:09 +0000

    response_clash_when_position_has_been_checked_by_other_rocket

--- RocketPlatform.Tests/RocketPlatformShould.cs
+++ RocketPlatform.Tests/RocketPlatformShould.cs
@@ -3,14 +3,19 @@ using NUnit.Framework;
 
 namespace RocketPlatform.Tests {
     public class RocketPlatformShould {
+        private Platform platform;
+        private Position position;
+
+        [SetUp]
+        public void Setup() {
+            platform = new Platform();
+        }
 
         [TestCase(5,5)]
         [TestCase(5,15)]
         [TestCase(15,5)]
         [TestCase(15,15)]
         public void response_ok_when_rocket_ask_for_a_position_whithin_the_boundaries(int x, int y) {
-
-            var platform = new Platform();
 
             var response = platform.CanILandOn(new Position(x, y));
 
@@ -23,11 +28,19 @@ namespace RocketPlatform.Tests {
         [TestCase(15,16)]
         public void response_out_when_rocket_ask_for_a_position_out_of_the_boundaries(int x, int y) {
 
-            var platform = new Platform();
-
             var response = platform.CanILandOn(new Position(x, y));
             
             response.Should().Be("out of platform");
+        }
+
+        [Test]
+        public void response_clash_when_position_has_been_checked() {
+            position = new Position(6, 6);
+            platform.CanILandOn(position);
+
+            var response = platform.CanILandOn(position);
+            
+            response.Should().Be("clash");
         }
     }
 }
