fa6049e442753f1885402f6c942341e39a8b1f7a
Emilio Medina <medinarribas@gmail.com>
2021-03-21 17:35:01 +0000

    extract platform instance to test setup

--- RocketPlatform.Tests/RocketPlatformShould.cs
+++ RocketPlatform.Tests/RocketPlatformShould.cs
@@ -3,14 +3,19 @@ using NUnit.Framework;
 
 namespace RocketPlatform.Tests {
     public class RocketPlatformShould {
+        private Platform platform;
+
+        [SetUp]
+        public void Setup() {
+            platform = new Platform();
+        }
+
 
         [TestCase(5,5)]
         [TestCase(5,15)]
         [TestCase(15,5)]
         [TestCase(15,15)]
         public void response_ok_when_rocket_ask_for_a_position_whithin_the_boundaries(int x, int y) {
-
-            var platform = new Platform();
 
             var response = platform.CanILandOn(new Position(x, y));
 
@@ -22,8 +27,6 @@ namespace RocketPlatform.Tests {
         [TestCase(16,15)]
         [TestCase(15,16)]
         public void response_out_when_rocket_ask_for_a_position_out_of_the_boundaries(int x, int y) {
-
-            var platform = new Platform();
 
             var response = platform.CanILandOn(new Position(x, y));
             
